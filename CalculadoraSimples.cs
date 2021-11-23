26b5c8a5a3a8b8ab6f1aa83b8793790cb67af983
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2021-11-23 20:45:49 -0300

    Add project files.

---
+++ CalculadoraSimples.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCalculadora
+{
+    public class CalculadoraSimples
+    {
+        /* Método para Adicionar 2 números */
+        public int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        /* Método para Multiplicar 2 números */
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        /* Método para Dividir 2 números */
+        public int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        /* Método para Diminuir 2 números */
+        public int Diminuir(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
